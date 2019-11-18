625f7fcb02c61d891f60f3b070774691a1e670cf
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 04:03:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   counter = 0
   while counter < array.length do
-    Math.sqrt(array[counter])
+    puts Math.sqrt(array[counter])
     counter++
   end
 end
